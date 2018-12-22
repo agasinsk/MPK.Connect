e142cc852b057ca9867632abf49d3eae4f89541f
Artur Gasiński <arturgasinski@hotmail.com>
2018-12-22 12:50:34 +0100

    Refactored TravelPlan service

--- MPK.Connect.Service/Business/TravelPlanService.cs
+++ MPK.Connect.Service/Business/TravelPlanService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using MoreLinq.Extensions;
+using MPK.Connect.Model.Business;
 using MPK.Connect.Model.Business.TravelPlan;
+using MPK.Connect.Model.Graph;
 using MPK.Connect.Service.Business.Graph;
 
 namespace MPK.Connect.Service.Business
@@ -29,23 +31,33 @@ namespace MPK.Connect.Service.Business
 
             if (string.IsNullOrEmpty(source.Name) || string.IsNullOrEmpty(destination.Name))
             {
-                var stops = graph.Nodes.Select(st => st.Value.Data.StopDto).DistinctBy(s => s.Id).ToList();
-                if (string.IsNullOrEmpty(source.Name))
-                {
-                    var sourceStop = stops.Aggregate((l, r) =>
-                        l.GetDistanceTo(source.Latitude.Value, source.Longitude.Value) <
-                        r.GetDistanceTo(source.Latitude.Value, source.Longitude.Value)
-                            ? l : r);
-                    source.Name = sourceStop.Name;
-                }
-                if (string.IsNullOrEmpty(destination.Name))
-                {
-                    var destinationStop = stops.Aggregate((l, r) => l.GetDistanceTo(destination.Latitude.Value, destination.Longitude.Value) < r.GetDistanceTo(destination.Latitude.Value, destination.Longitude.Value) ? l : r);
-                    destination.Name = destinationStop.Name;
-                }
+                UpdatedLocationsWithNamesOfTheClosestStops(source, destination, graph);
             }
 
             return _travelPlanProvider.GetTravelPlans(graph, source, destination);
+        }
+
+        private StopDto GetClosestStop(Location source, List<StopDto> stops)
+        {
+            return stops.Aggregate((l, r) =>
+                l.GetDistanceTo(source.Latitude.Value, source.Longitude.Value) <
+                r.GetDistanceTo(source.Latitude.Value, source.Longitude.Value)
+                    ? l : r);
+        }
+
+        private void UpdatedLocationsWithNamesOfTheClosestStops(Location source, Location destination, Graph<string, StopTimeInfo> graph)
+        {
+            var stops = graph.Nodes.Select(st => st.Value.Data.StopDto).DistinctBy(s => s.Id).ToList();
+            if (string.IsNullOrEmpty(source.Name))
+            {
+                var sourceStop = GetClosestStop(source, stops);
+                source.Name = sourceStop.Name;
+            }
+            if (string.IsNullOrEmpty(destination.Name))
+            {
+                var destinationStop = GetClosestStop(destination, stops);
+                destination.Name = destinationStop.Name;
+            }
         }
 
         private void ValidateLocations(params Location[] locations)
